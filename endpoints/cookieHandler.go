b6d5ec9fb8d45c7b23d8cfedf928534ee1651467
Steffen Sæthre Martinsen <steffema@stud.ntnu.no>
2024-06-11 20:45:34 +0200

    added cookie set and get logic in the backend

---
+++ endpoints/cookieHandler.go
@@ -0,0 +1,77 @@
+package endpoints
+
+import (
+	"dashboard/utils"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"time"
+)
+
+// SetCookie is a function to set a cookie for a user
+func SetCookie(w http.ResponseWriter, r *http.Request) {
+
+	// Ensure the request method is POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		log.Println("Invalid request method")
+		return
+	}
+
+	// Read the request body
+	user := utils.UserCookie{}
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Error decoding POST request", http.StatusBadRequest)
+		log.Println("Error decoding Cookie POST request")
+		return
+	}
+
+	// Generate a random token
+	token, err := utils.GenerateRandomToken(32)
+	if err != nil {
+		http.Error(w, "Error generating token", http.StatusInternalServerError)
+		log.Println("Error generating token")
+		return
+	}
+
+	log.Println("Token: " + token)
+
+	cookie := &http.Cookie{
+		Name:     user.Username,
+		Value:    token,
+		Expires:  time.Now().Add(24 * time.Hour),
+		Domain:   "localhost",
+		Path:     "/",
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, cookie)
+	w.WriteHeader(http.StatusOK)
+	log.Println("Cookie for user '" + cookie.Name + "' set")
+
+}
+
+// GetCookie is a function to get a cookie for a user
+func GetCookie(w http.ResponseWriter, r *http.Request) {
+
+	// Extract the username from the request path
+	utils.EnsureCorrectPath(r)
+	username := utils.ExtractUsername(w, r)
+
+	cookie, err := r.Cookie(username)
+	if err != nil {
+		switch {
+		case errors.Is(err, http.ErrNoCookie):
+			log.Println("No cookie found for user '" + username + "'")
+
+		default:
+			log.Println("Error getting cookie for user '" + username + "'.")
+		}
+		return
+	}
+
+	log.Println("Cookie for user '" + cookie.Name + "' found")
+
+}
